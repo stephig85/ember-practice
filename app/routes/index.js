899882928dc22473e6c7e49c52f1ddf63056790b
Stephanie Gage <info@stephaniegage.com>
2017-04-04 10:13:43 -0500

    added some hard-coded listings to rentals page (will be changed later)

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // This sets the path to show the "rentals.hbs" upon loading the index.  It replaces '/' with '/rentals'
   beforeModel() {
   this.replaceWith('rentals');
 }
