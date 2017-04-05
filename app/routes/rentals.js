fadb4034b087bec29d2f94bff80fe4e14d96cd86
Stephanie Gage <info@stephaniegage.com>
2017-04-05 11:09:22 -0500

    built a simple component; used ember data and handlebar helper

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -2,35 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return [{
-       id: 'grand-old-mansion',
-       title: 'Grand Old Mansion',
-       owner: 'Veruca Salt',
-       city: 'San Francisco',
-       type: 'Estate',
-       bedrooms: 15,
-       image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-       description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
-     }, {
-       id: 'urban-living',
-       title: 'Urban Living',
-       owner: 'Mike TV',
-       city: 'Seattle',
-       type: 'Condo',
-       bedrooms: 1,
-       image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
-       description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
+// call the findAll function on the store and provide it with the name of your newly created rental model class.
+// When we call findAll, Ember Data will attempt to fetch rentals from /api/rentals. If you recall, in the section titled Installing Addons we set up an adapter to route data requests through /api.
+// Since we have already set up Ember Mirage in our development environment, Mirage will return the data we requested without actually making a network request.
+// When we deploy our app to a production server, we will likely want to replace Mirage with a remote server for Ember Data to communicate with for storing and retrieving persisted data. A remote server will allow for data to be shared and updated across users.
 
-     }, {
-       id: 'downtown-charm',
-       title: 'Downtown Charm',
-       owner: 'Violet Beauregarde',
-       city: 'Portland',
-       type: 'Apartment',
-       bedrooms: 3,
-       image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
-       description: 'Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet.'
-
-     }];
+    return this.get('store').findAll('rental');
    }
  });
